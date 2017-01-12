92058c439e79255b9ad63298764e6bc091fc30cc
Marcus Nordberg <nordberg@github.com>
2017-01-12 16:56:11 +0100

    Do not return stream

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 
 gulp.task('pug', function(done) {
-  return gulp.src('views/*.pug')
+  gulp.src('./views/**/*.pug')
   .pipe(pug())
-  .pipe(gulp.dest('./www/app/'))
+  .pipe(gulp.dest('./public'))
   .on('end', done);
 });
 
 gulp.task('default', ['pug']);
 
 gulp.task('watch', function() {
-  gulp.watch('.views/*.pug', ['pug']);
+  gulp.watch('./views/**/*.pug', ['pug']);
 })
